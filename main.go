148a5ebc1b5ed705547c799fff23c507afbaf2e9
mimaken <ken0930mata@ezweb.ne.jp>
2020-01-31 13:53:32 +0900

    GAEにデプロイ出来るようにした

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/labstack/echo"
+	"github.com/mimaken3/ShareIT-api/application/server"
+	"google.golang.org/appengine"
+)
+
+func main() {
+	e := echo.New()
+
+	server.InitRouting(e)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		e.Logger.Printf("Defaulting to port %s", port)
+	}
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", port)))
+	// e.Logger.Fatal(e.Start(":8080"))
+	appengine.Main()
+}
