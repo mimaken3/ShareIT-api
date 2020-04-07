29d4072b3eb964ff266a1dd69cded3d323e974b1
mimaken <ken0930mata@ezweb.ne.jp>
2020-04-07 10:59:07 +0900

    特定のユーザの全記事取得

--- infrastructure/article.go
+++ infrastructure/article.go
@@ -129,7 +129,19 @@ func (articleRepo *articleInfraStruct) UpdateArticleByArticleId(willBeUpdatedArt
 
 // 特定のユーザの全記事を取得
 func (articleRepo *articleInfraStruct) FindArticlesByUserId(userID uint) (articles []model.Article, err error) {
-	articleRepo.db.Where("created_user_id = ? AND is_deleted = ? ", userID, 0).Find(&articles)
+	rows, err :=
+		articleRepo.db.Raw("select a.article_id, a.article_title, a.created_user_id, a.article_content, group_concat(att.topic_id order by att.article_topic_id) "+
+			"as article_topics, a.created_date, a.updated_date, a.deleted_date from articles as a, article_topics as att where a.article_id = att.article_id "+
+			"and a.created_user_id = ? and is_deleted = 0 group by  a.article_id", userID).Rows()
+
+	defer rows.Close()
+	for rows.Next() {
+		article := model.Article{}
+		err = articleRepo.db.ScanRows(rows, &article)
+		if err == nil {
+			articles = append(articles, article)
+		}
+	}
 
 	// レコードがない場合
 	if len(articles) == 0 {
